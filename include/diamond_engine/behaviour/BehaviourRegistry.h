aea7406c9fa4a390938a7fba6e38b73a6d6f5886
KarimTakieddine <karim.m.takieddine@gmail.com>
2024-05-09 23:02:35 +0100

    More file deletion

--- include/diamond_engine/behaviour/BehaviourRegistry.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include "GameObjectBuilder.h"
-#include "GameObjectConfigParser.h"
-
-namespace diamond_engine {
-void RegisterBehaviour(const std::string& name, GameObjectConfigParser::BehaviourParseFunc parseFunc, GameObjectBuilder::BehaviourBuildFunc buildFunc);
-}
